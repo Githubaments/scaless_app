ce702e19f83b2e7553f64d938473836e07717e07
Githubaments <flynnshane@gmail.com>
2023-09-15 18:00:51 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,56 +1,45 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
-@st.cache
-def load_data(file_path):
-    return pd.read_csv(file_path)
+# Load data
+def load_data(uploaded_file):
+    df = pd.read_csv(uploaded_file)
+    df['Time of Measurement'] = pd.to_datetime(df['Time of Measurement'])
+    df['Fat (kg)'] = (df['Body Fat(%)'] / 100) * df['Weight(kg)']
+    df['30-day MA Weight'] = df['Weight(kg)'].rolling(window=30).mean()
+    df['90-day Exponential Smoothing Weight'] = df['Weight(kg)'].ewm(span=90).mean()
+    return df
 
-def main():
-    st.title("Personal Health Dashboard")
+st.title('Weight and Health Stats Analysis')
 
-    file_path = st.file_uploader("Upload your personal data CSV file", type=["csv"])
-    if file_path is not None:
-        df = load_data(file_path)
-        st.dataframe(df)
+uploaded_file = st.sidebar.file_uploader("Choose a CSV file", type="csv")
+if uploaded_file is not None:
+    df = load_data(uploaded_file)
 
-        # Choose the header to plot
-        selected_header = st.selectbox("Select a header to plot over time", df.columns)
+    # Sidebar
+    st.sidebar.header('Settings')
+    selected_metrics = st.sidebar.multiselect(
+        'Select metrics to plot', 
+        options=['Weight(kg)', 'Fat (kg)', '30-day MA Weight', '90-day Exponential Smoothing Weight', 'BMI', 'Body Fat(%)'],
+        default=['Weight(kg)']
+    )
 
-        # Plotting the selected header over time using Plotly
-        fig = px.line(df, x='Time of Measurement', y=selected_header)
+    # Main
+    if selected_metrics:
+        fig = go.Figure()
+        for metric in selected_metrics:
+            if metric == 'Weight(kg)':
+                fig.add_trace(go.Scatter(x=df['Time of Measurement'], y=df[metric], mode='markers', name=metric))
+            else:
+                fig.add_trace(go.Scatter(x=df['Time of Measurement'], y=df[metric], mode='lines', name=metric))
+        fig.update_layout(xaxis_title="Date")
         st.plotly_chart(fig)
+    else:
+        st.write("Please select at least one metric to display.")
+else:
+    st.write("Please upload a CSV file to proceed.")
 
-        # Showing some statistics
-        st.write("### Personal Data Statistics")
-        st.write("Mean value: ", df[selected_header].mean())
-        st.write("Minimum value: ", df[selected_header].min())
-        st.write("Maximum value: ", df[selected_header].max())
-
-    google_fit_path = st.file_uploader("Upload your Google Fit data CSV file", type=["csv"])
-    if google_fit_path is not None:
-        df_google_fit = load_data(google_fit_path)
-        st.dataframe(df_google_fit)
-
-        # Choose the header to plot
-        selected_header_google_fit = st.selectbox("Select a header to plot over time", df_google_fit.columns)
-
-        # Plotting the selected header over time using Plotly
-        fig_google_fit = px.line(df_google_fit, x='Time of Measurement', y=selected_header_google_fit)
-        st.plotly_chart(fig_google_fit)
-
-        # Showing some statistics
-        st.write("### Google Fit Data Statistics")
-        st.write("Mean value: ", df_google_fit[selected_header_google_fit].mean())
-        st.write("Minimum value: ", df_google_fit[selected_header_google_fit].min())
-        st.write("Maximum value: ", df_google_fit[selected_header_google_fit].max())
-
-        # 7-day moving average of heart points
-        df_google_fit['7-day avg heart points'] = df_google_fit['Heart Points'].rolling(window=7).mean()
-        fig_7day_avg = px.line(df_google_fit, x='Time of Measurement', y='7-day avg heart points')
-        st.plotly_chart(fig_7day_avg)
-
-        # Count of days with more than 10,000 steps
-        df_google_fit['over_10000'] = df_google_fit['Steps'] >= 10000
-        fig_steps = px.bar(df_google_fit, x='Time of Measurement', y='over_10000', color='over_10000')
-       
+if __name__ == '__main__':
+    pass
