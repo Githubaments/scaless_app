005bdb79c9e8775d7cd15bcdfddebcfc65046609
Githubaments <flynnshane@gmail.com>
2023-09-15 18:12:53 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -10,6 +10,10 @@ def load_data(uploaded_file):
     df['Fat (kg)'] = (df['Body Fat(%)'] / 100) * df['Weight(kg)']
     df['30-day MA Weight'] = df['Weight(kg)'].rolling(window=30).mean()
     df['90-day Exponential Smoothing Weight'] = df['Weight(kg)'].ewm(span=90).mean()
+    df['30-day MA Body Fat(%)'] = df['Body Fat(%)'].rolling(window=30).mean()
+    df['90-day Exponential Smoothing Body Fat(%)'] = df['Body Fat(%)'].ewm(span=90).mean()
+    df['30-day MA Fat (kg)'] = df['Fat (kg)'].rolling(window=30).mean()
+    df['90-day Exponential Smoothing Fat (kg)'] = df['Fat (kg)'].ewm(span=90).mean()
     return df
 
 st.title('Weight and Health Stats Analysis')
@@ -18,7 +22,7 @@ uploaded_file = st.sidebar.file_uploader("Choose a CSV file", type="csv")
 if uploaded_file is not None:
     df = load_data(uploaded_file)
 
-    # Sidebar
+    # Sidebar for Weight Analysis
     st.sidebar.header('Settings for Weight Analysis')
     selected_metrics_weight = st.sidebar.multiselect(
         'Select metrics to plot', 
@@ -30,7 +34,7 @@ if uploaded_file is not None:
     st.header('Weight Analysis')
     fig_weight = go.Figure()
     for metric in selected_metrics_weight:
-        if metric == 'Weight(kg)':
+        if metric in ['Weight(kg)', 'Fat (kg)']:
             fig_weight.add_trace(go.Scatter(x=df['Time of Measurement'], y=df[metric], mode='markers', opacity=0.5, name=metric))
         else:
             fig_weight.add_trace(go.Scatter(x=df['Time of Measurement'], y=df[metric], mode='lines', name=metric))
@@ -41,15 +45,15 @@ if uploaded_file is not None:
     st.sidebar.header('Settings for Body Fat Analysis')
     selected_metrics_fat = st.sidebar.multiselect(
         'Select metrics for Body Fat Analysis', 
-        options=['Body Fat(%)', 'Fat (kg)'],
-        default=['Body Fat(%)']
+        options=['Body Fat(%)', 'Fat (kg)', '30-day MA Body Fat(%)', '90-day Exponential Smoothing Body Fat(%)', '30-day MA Fat (kg)', '90-day Exponential Smoothing Fat (kg)'],
+        default=['Body Fat(%)', '30-day MA Body Fat(%)', '90-day Exponential Smoothing Body Fat(%)']
     )
 
     # Main Body Fat Analysis
     st.header('Body Fat Analysis')
     fig_fat = go.Figure()
     for metric in selected_metrics_fat:
-        if metric == 'Body Fat(%)':
+        if metric in ['Body Fat(%)', 'Fat (kg)']:
             fig_fat.add_trace(go.Scatter(x=df['Time of Measurement'], y=df[metric], mode='markers', opacity=0.5, name=metric))
         else:
             fig_fat.add_trace(go.Scatter(x=df['Time of Measurement'], y=df[metric], mode='lines', name=metric))
@@ -61,4 +65,6 @@ else:
 
 if __name__ == '__main__':
     pass
+
+
 
